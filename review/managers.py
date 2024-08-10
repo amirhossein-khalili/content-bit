f4845a9e168e1943c1e85f976c10573e79edbdf9
amir <amir1378khalili@gmail.com>
2024-08-10 16:25:28 +0330

    fix typo

--- review/managers.py
+++ review/managers.py
@@ -4,7 +4,7 @@ from .models import Review
 
 
 class ReviewManager(models.Manager):
-    def get_tags_for(self, obj_type, obj_id):
+    def get_reviews_for(self, obj_type, obj_id):
         content_type = ContentType.objects.get_for_model(obj_type)
 
         return Review.objects.filter(content_type=content_type, object_id=obj_id)
