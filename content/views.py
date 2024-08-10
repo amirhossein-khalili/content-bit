f3b4d6966fc17bd5e1e29ebdca4d9ac3a23e031b
amir <amir1378khalili@gmail.com>
2024-08-10 23:47:27 +0330

    add ArticleListView

--- content/views.py
+++ content/views.py
@@ -1,3 +1,27 @@
+from django.db.models import Avg
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from review.models import Review
+
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleListView(APIView):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = ArticleSerializer
+
+    def get(self, request):
+
+        # articles = Article.objects.all()
+
+        articles = Article.objects.annotate(avg_rating=Avg("reviews__rating"))
+
+        serializer = self.serializer_class(articles, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
