c6f17c27449ad2114cfab8e7f942b2f885924bf3
amir <amir1378khalili@gmail.com>
2024-08-10 15:39:38 +0330

    ENHANCE review model

--- review/models.py
+++ review/models.py
@@ -1,11 +1,27 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.models import ContentType, Q
 from django.db import models
+
+from .managers import ReviewManager
 
 
 class Review(models.Model):
+    objects = ReviewManager()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+    rating = models.IntegerField()
+
+    class Meta:
+        unique_together = ("content_type", "user_id", "object_id")
+        constraints = [
+            models.CheckConstraint(
+                check=Q(rating__gte=0) & Q(rating__lte=5),
+                name="rating_range",
+            )
+        ]
+
+    def __str__(self):
+        return f"Review of {self.object_id} from {self.content_type} by {self.user_id} : {self.rating}"
