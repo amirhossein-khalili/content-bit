66463441f636ccc3934e2504698b92035faf0004
amir <amir1378khalili@gmail.com>
2024-08-11 17:25:32 +0330

    add pagination to list items

--- content/views.py
+++ content/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Avg, OuterRef, Subquery
 from django.shortcuts import render
 from rest_framework import generics, status
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,7 +10,30 @@ from rest_framework.views import APIView
 from review.models import Review
 
 from .models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, ReviewCreateSerializer
+
+# class ArticleListView(APIView):
+
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = ArticleSerializer
+
+#     def get(self, request):
+#         user = request.user
+
+#         # Annotate each article with the average rating
+#         articles = Article.objects.annotate(
+#             avg_rating=Avg("reviews__rating"),
+#             user_rating=Subquery(
+#                 Review.objects.filter(
+#                     content_type=OuterRef("reviews__content_type"),
+#                     object_id=OuterRef("id"),
+#                     user=user,
+#                 ).values("rating")[:1]
+#             ),
+#         )
+#         serializer = self.serializer_class(articles, many=True)
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ArticleListView(APIView):
@@ -30,9 +55,17 @@ class ArticleListView(APIView):
                 ).values("rating")[:1]
             ),
         )
-        serializer = self.serializer_class(articles, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # Set up pagination
+        paginator = PageNumberPagination()
+        paginator.page_size = 10  # Number of articles per page
+        paginated_articles = paginator.paginate_queryset(articles, request)
+
+        # Serialize the data
+        serializer = self.serializer_class(paginated_articles, many=True)
+
+        # Return paginated response
+        return paginator.get_paginated_response(serializer.data)
 
 
 class AddArticleRatingView(APIView):
