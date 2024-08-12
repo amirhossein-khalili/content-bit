c6b614b9b22d852aca2260c4e1bda1cb998a386c
amir <amir1378khalili@gmail.com>
2024-08-12 14:00:21 +0330

    ADD slug to Article

--- content/models.py
+++ content/models.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.utils.text import slugify
 
 from review.models import Review
 
@@ -7,7 +8,7 @@ from review.models import Review
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    slug = models.SlugField(max_length=20, default=None)
+    slug = models.SlugField(max_length=20, unique=True, blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
@@ -15,3 +16,13 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)[:5]
+            counter = 1
+            original_slug = self.slug
+            while Article.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
+        super().save(*args, **kwargs)
