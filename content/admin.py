15714611fc5262a8d1dcda1c80c85d30595b9605
amir <amir1378khalili@gmail.com>
2024-08-13 12:55:29 +0330

    add review to admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Review
 
 
+@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(Article, ArticleAdmin)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
