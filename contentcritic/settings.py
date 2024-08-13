fb6be8f00299c1f39b32ade1fbba1074893e052f
amir <amir1378khalili@gmail.com>
2024-08-13 13:24:48 +0330

    add pagination to REST FRAME WORK settings

--- contentcritic/settings.py
+++ contentcritic/settings.py
@@ -160,7 +160,8 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.UserRateThrottle",
     ],
     "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
-    # "DEFAULT_METADATA_CLASS": "contentcritic.metadata.CustomMetadata",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 
